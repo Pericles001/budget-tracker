2d791a6b524096c625520fb6b03d0d4782cb375a
Pericles ADJOVI <55035365+Pericles001@users.noreply.github.com>
2021-03-15 13:31:41 +0100

    Add expense form Created add expense form component and called it in app.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,14 +5,17 @@ const ExpenseList = () => {
   const expenses = [
     { id: 12, name: "shopping", cost: 40 },
     { id: 13, name: "holiday", cost: 400 },
-    { id: 14, name: "car service", cost: 50 }
+    { id: 14, name: "car service", cost: 50 },
+    { id: 15, name: "Transportation", cost: 500 },
+    { id: 13, name: "Fuel", cost: 400 },
+    { id: 14, name: "Child care", cost: 250 }
   ];
 
   return (
-    <ul className="list-group">
-      {expenses.map((expense) => {
-        <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />;
-      })}
+    <ul>
+      {expenses.map((expense) => (
+        <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
+      ))}
     </ul>
   );
 };
