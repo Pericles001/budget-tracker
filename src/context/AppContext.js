0a756046b1e2ff0e7f798ca84d54a4d01bf4fba9
Pericles ADJOVI <55035365+Pericles001@users.noreply.github.com>
2021-03-15 13:53:54 +0100

    App provider in App context created app provider feature and wrap every return contents into it

---
+++ src/context/AppContext.js
@@ -0,0 +1,37 @@
+import { createContext, useReducer } from "react";
+
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  budget: 2000,
+  expenses: [
+    { id: 12, name: "shopping", cost: 40 },
+    { id: 13, name: "holiday", cost: 400 },
+    { id: 14, name: "car service", cost: 50 },
+    { id: 15, name: "Transportation", cost: 500 },
+    { id: 13, name: "Fuel", cost: 400 },
+    { id: 14, name: "Child care", cost: 250 }
+  ]
+};
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  return (
+    <AppContext.Provider
+      value={{
+        budget: state.budget,
+        expenses: state.expenses,
+        dispatch
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  );
+};
